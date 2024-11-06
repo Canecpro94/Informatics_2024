3dc41ffd4a5cd0bce207c8b26ae10fe87d85fb23
GypsyGang <b.artem04@mail.ru>
2024-11-06 13:04:10 +0300

    lab6

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	lab6 "isuct.ru/informatics2022/laba6"
+)	
 func main() {
-	fmt.Println("Hello world")
-}
+	fmt.Println("Луховской Александр Германович")
+
+	lab6.Laba6Run()
